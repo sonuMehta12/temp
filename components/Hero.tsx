8cace065cab0e2f36b32f0ecfe8f99195d0c80d9
Sonu Kumar <sonu12022000@gmail.com>
2025-11-13 13:24:11 +0530

    changes2

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,11 +2,11 @@ import type React from 'react';
 
 export const Hero: React.FC = () => {
     return (
-        <header className="text-center">
-            <h1 className="text-4xl sm:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-cyan-500">
-                ET Sector Simulator
+        <header className="text-center h-1/2">
+            <h1 className="text-4xl px-4 sm:px-6 sm:text-5xl font-extrabold bg-gradient-to-r from-indigo-600 via-purple-600 to-cyan-500 bg-clip-text text-transparent leading-tight py-2">
+               How the budget impacts YOU!
             </h1>
-            <p className="mt-4 max-w-2xl mx-auto text-lg text-gray-600">
+            <p className="mt-3 sm:mt-6 max-w-2xl mx-auto text-lg text-gray-600">
                 Get a personalized 5-point brief on how the latest budget impacts you. Select your sector and role to begin.
             </p>
         </header>
